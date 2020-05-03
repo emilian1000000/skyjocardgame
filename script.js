e401560cb698cd8dc9f0581af93c0dbd3db13cf9
emilian1000000 <64052572+emilian1000000@users.noreply.github.com>
2020-05-03 13:47:04 -0400

    May 3, 2020

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// tooling
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
 //Step 1: Ask names of players
 const templates = {
   playername: document.getElementById("player-name"),
@@ -17,56 +5,61 @@ const templates = {
 };
 const players = document.getElementById("players");
 const gameboard = document.getElementById("gameboard");
+const deck = document.getElementById("deck");
+const open_card = document.getElementById("open-card");
 for (let i = 0; i < 8; i++) {
   const clone = templates.playername.content.cloneNode(true);
   players.appendChild(clone);
 }
-//Step 2: Activate game via button
 function begingame(event) {
   while (gameboard.hasChildNodes())
     gameboard.removeChild(gameboard.firstChild);
-  //Step 3: Shuffle the cards
-  const cards = [-2, -2, -2, -2, -2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12];
-  shuffle(cards);
-  //Step 4: Count players
-  const players = [];
-  document.forms.players.playernames.forEach(input => {
-    if (input.value != '')
-      players.push(input.value);
-  });
-  //Step 5: deal 12 'cards' to each player and 'arrange' into a rectangle of 4x3 while removing the numbers/cards that we place from the "cards" array
-  shuffle(players);
-  const playerscards = [];
-  for (let i = 0; i < 12; i++) {
-    for (let j in players) {
-      if (i == 0)
-        playerscards[j] = [];
-      playerscards[j].push({
-        card: cards.pop(), 
-        open: false
-      });
+  const gAmE = new Game(
+    Array.from(document.forms.players.playernames, i => i.value).filter(i => i != "")
+  );
+  gAmE.dealcards();
+  function playerwantstoopencard(playerIndex, cardIndex) {
+    console.log(`player ${playerIndex} wants to open card ${cardIndex}`);
+    if (gAmE.player_cards_open(playerIndex) < 2) {
+      cardflip(playerIndex, cardIndex);
     }
   }
-  //console.log(playerscards);
-  for (let i in players) {
+  function make_player_board(playername, i) {
     const clone = templates.playercards.content.cloneNode(true);
-    clone.getElementById("gbname").textContent = players[i];
+    clone.getElementById("gbname").textContent = playername;
     const table = clone.querySelector("table");
     table.id = `board${i}`;
-    if (i % 2 == 1) {
+    const totalpoints = clone.getElementById("totalpoints");
+    totalpoints.id = `totalpoints${i}`;
+    if (i % 2 == 1) 
       table.classList.add("altbackground");
+    const cardbuttons = table.querySelectorAll("button[name='cardbutton']");
+    for (let j in cardbuttons) {
+      cardbuttons[j].onclick = event => playerwantstoopencard(i, j);
     }
     gameboard.appendChild(clone);
   }
-  //Step 6: Let each player flip 2 cards over from their 12
+  gAmE.playernames.forEach(make_player_board);
+  function showlastopencard() {
+    lastopen = gAmE.last_open();
+    open_card.textContent = lastopen;
+    open_card.classList.value = `cardbutton ${cardbackgrounds[lastopen]}`;
+  }
+  showlastopencard();
   function cardflip(playerIndex, cardIndex) {
-    if (!playerscards[playerIndex][cardIndex].open) {
-      playerscards[playerIndex][cardIndex].open = true;
+    if (gAmE.open_if_closed(playerIndex, cardIndex)) {
       const playerboard = document.getElementById(`board${playerIndex}`);
       const cardbutton = playerboard.querySelectorAll("button[name='cardbutton']")[cardIndex];
-      cardbutton.textContent = playerscards[playerIndex][cardIndex].card.toString();
+      cardbutton.textContent = gAmE.player_card(playerIndex, cardIndex);
+      cardbutton.classList.add(cardbackgrounds[gAmE.player_card(playerIndex, cardIndex)]);
+
+      const totalpoints = document.getElementById(`totalpoints${playerIndex}`);
+      totalpoints.textContent = gAmE.player_points(playerIndex).toString();
     }
   }
-  //cardflip(0, 0); //just for tests
-  document.getElementById("board0").classList.add("turn");
+  while (!gAmE.is_debut_complete())
+    wait();
+  //cardflip(0, 0); just for tests
+
+  //document.getElementById("board0").classList.add("turn");
 }
